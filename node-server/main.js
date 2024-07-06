cf2a8fd298bbf8d5f537960e7e5baf0826ab8a37
Theo Ephraim <theo@dmno.dev>
2024-07-06 18:38:35 +0200

    using node

---
+++ node-server/main.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+
+
+const upload = multer({ dest: 'uploads' });
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "uploads/");
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, file.fieldname + "-" + Date.now() + ".m4a");
+//     },
+//   })
+// });
+
+app.use(cors());
+app.use(express.static('static'));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // allow any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Routes
+app.post("/upload", upload.single("audio"), (req, res) => {
+  console.log(req.file)
+  console.log("Received file: " + req.file.filename);
+  res.json({
+    uploadId: req.file.filename,
+    message: "Successfully uploaded " + req.file.filename
+  });
+});
+
+app.get("/play", async (req, res) => {
+  const formData = new FormData();
+  formData.append(
+    "text",
+    "Thank you for lending me your voice. Over time, I will start to sound more and more like you.",
+  );
+  //change stephen.m4a to outaudio.mp4
+  formData.append(
+    "speaker_ref_path",
+    `http://localhost:${PORT}/uploads/${req.query.uploadId}`,
+  );
+  formData.append("guidance", "5.0");
+  formData.append("top_p", "0.98");
+  
+  try {
+    const response = await fetch("http://0.0.0.0:58003/tts", {
+      method: "POST",
+      body: formData,
+    });
+    res.send(response.body);
+  } catch (err) {
+    res.status(400);
+    res.json({
+      message: 'failed to play',
+    })
+    console.log('failed :(');
+  }
+})
+
+
+app.get("/:universalURL", (req, res) => {
+  res.send("404 URL NOT FOUND");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
--- voiceapp.py
+++ voiceapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_from_directory
 import os
 import subprocess
+import requests
 
 app = Flask(__name__)
 
@@ -39,6 +40,28 @@ def convert_to_mp4(webm_path, mp4_path):
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/tts')
+def proxy_tts():
+    res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+        method          = request.method,
+        url             = 'http://0.0.0.0:58003/tts',
+        headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
+        data            = request.get_data(),
+        # cookies         = request.cookies,
+        # allow_redirects = False,
+    )
+
+    #region exlcude some keys in :res response
+    # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']  #NOTE we here exclude all "hop-by-hop headers" defined by RFC 2616 section 13.5.1 ref. https://www.rfc-editor.org/rfc/rfc2616#section-13.5.1
+    # headers          = [
+    #     (k,v) for k,v in res.raw.headers.items()
+    #     if k.lower() not in excluded_headers
+    # ]
+    #endregion exlcude some keys in :res response
+
+    response = Response(res.content, res.status_code, headers)
+    return response
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
